4a833ba10643f50ae299155c4e85459cdc9f3228
Andy <Andy@ANDY>
2026-03-22 01:52:14 -0600

    ejercicio 7

---
+++ 7/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _7
+{
+    internal class Program
+    {
+        //Mostrar el doble de un número utilizando una función MostrarDoble(int n).
+         static void MostrarDoble(int n)
+        {
+            Console.WriteLine($"El doble de {n} es {n * 2}");
+        }
+        static void Main(string[] args)
+        {
+          MostrarDoble(5);
+        }
+    }
+}
