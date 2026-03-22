07f6ebf7a851027cea192b5fb46c526848cd8985
Andy <Andy@ANDY>
2026-03-22 08:31:05 -0600

    ejercicio 14

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        //Dividir dos números utilizando una función Dividir(double a, double b) que retorne el resultado
+        static void Dividir(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("No se puede dividir por cero.");
+                return;
+            }
+            double resultado = a / b;
+            Console.WriteLine($"El resultado de dividir {a} entre {b} es: {resultado}");
+        }
+        static void Main(string[] args)
+        {
+            Dividir(0,5);
+        }
+    }
+}
