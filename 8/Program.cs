6083a8a4ac80ef5bf1aaddc6128f93b48effd18b
Andy <Andy@ANDY>
2026-03-22 01:57:37 -0600

    ejercicio 8

---
+++ 8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _8
+{
+    internal class Program
+    {
+        //Mostrar el cuadrado de un número utilizando una función MostrarCuadrado(int n)
+        static void MostrarCuadrado(int n)
+        {
+            Console.WriteLine($"El cuadrado de {n} es: {(n*n)}");
+        }
+        static void Main(string[] args)
+        {
+            MostrarCuadrado(3);
+        }
+    }
+}
