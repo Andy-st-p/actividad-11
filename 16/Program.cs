c0d69e918eb467901dee8beadafb3d176d8fb0fe
Andy <Andy@ANDY>
2026-03-22 08:54:20 -0600

    ejercicio 16

---
+++ 16/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _16
+{
+    internal class Program
+    {
+        //Determinar si un número es positivo utilizando una función EsPositivo(int n) que retorne verdadero o falso.
+        static bool EsPositivo(int n)
+        {
+            return n > 0;
+        }
+        static void Main(string[] args)
+        {
+            bool resultado= EsPositivo(-5);
+            Console.WriteLine(resultado);
+        }
+    }
+}
