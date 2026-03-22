439c1e896e2c138f8c6d05ea51bdb606f4a3c638
Andy <Andy@ANDY>
2026-03-22 01:12:56 -0600

    ejercicio 4 completado

---
+++ 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _4
+{
+    internal class Program
+    {
+        //Imprimir una línea separadora utilizando una función Linea() que muestre “----------------”.
+        static void FuncionLinea()
+        {
+            Console.WriteLine("----------------");
+        }
+        static void Main(string[] args)
+        {
+            FuncionLinea();
+        }
+    }
+}
