3cfa9e25a63f5ea077dae45d00e294c2c520d67a
Andy <Andy@ANDY>
2026-03-22 00:45:39 -0600

    Agregar archivos de proyecto.

---
+++ actividad 011/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace actividad_011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
