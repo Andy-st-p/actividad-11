470a206b285664753b86a3cc1a2ac7b137b1b08e
Andy <Andy@ANDY>
2026-03-22 09:01:37 -0600

    ejercicio 18

---
+++ 18/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _18
+{
+    internal class Program
+    {
+        //Determinar el menor de tres números utilizando una función MenorDeTres(int a, int b, int c) que retorne el valor menor.
+        static void MenorDeTres(int a, int b, int c)
+        {
+            int menor = a;
+            if (b < menor)
+            {
+                menor = b;
+            }
+            if (c < menor)
+            {
+                menor = c;
+            }
+            Console.WriteLine($"El número menor entre {a}, {b} y {c} es: {menor}");
+        }
+        static void Main(string[] args)
+        {
+            MenorDeTres(6,2,3);
+        }
+    }
+}
