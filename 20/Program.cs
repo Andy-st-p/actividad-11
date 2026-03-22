5450f71a18c8ef4d41fbb4da390f4414c558d40a
Andy <Andy@ANDY>
2026-03-22 09:12:56 -0600

    ejercicio 20

---
+++ 20/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _20
+{
+    internal class Program
+    {
+        //Contar la cantidad de caracteres de un texto utilizando una función ContarLetras(string texto).
+        static void ContarLetras(string texto)
+        {
+            Console.WriteLine($"El texto '{texto}' tiene {texto.Length} caracteres.");
+        }
+        static void Main(string[] args)
+        {
+            ContarLetras("domingo");
+        }
+    }
+}
