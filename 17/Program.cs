e70465ff0c3a81e9b603fff6ecbe9310976dd84c
Andy <Andy@ANDY>
2026-03-22 08:58:21 -0600

    ejercicio 17

---
+++ 17/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _17
+{
+    internal class Program
+    {
+        //Determinar el mayor de dos números utilizando una función Mayor(int a, int b) que retorne el valor mayor.
+        static void Mayor(int a, int b)
+        {
+            if (a > b)
+            {
+                Console.WriteLine($"El número mayor entre {a} y {b} es: {a}");
+            }
+            else if (b > a)
+            {
+                Console.WriteLine($"El número mayor entre {a} y {b} es: {b}");
+            }
+            else
+            {
+                Console.WriteLine($"Los números {a} y {b} son iguales.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Mayor(5,5);
+        }
+    }
+}
