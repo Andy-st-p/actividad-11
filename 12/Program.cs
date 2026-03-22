461699bdad55dd456fb8f468965b76d8785dd894
Andy <Andy@ANDY>
2026-03-22 08:23:37 -0600

    ejercicio 12

---
+++ 12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _12
+{
+    internal class Program
+    {
+        //Restar dos números utilizando una función Restar(int a, int b) que retorne el resultado.
+        static void Restar(int a, int b)
+        {
+            int resultado = a - b;
+            Console.WriteLine($"El resultado de restar {b} - {a} es: {resultado}");
+        }
+        static void Main(string[] args)
+        {
+            Restar(6,4);
+        }
+    }
+}
