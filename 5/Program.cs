d676f18633c0a37b0f1ea568b91b85c4a9443468
Andy <Andy@ANDY>
2026-03-22 01:15:20 -0600

    ejercicio 5 completado

---
+++ 5/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _5
+{
+    internal class Program
+    {
+        //Mostrar un menú simple utilizando una función MostrarMenu() con tres opciones.
+        static void MostrarMenu()
+        {
+            Console.WriteLine("---Menú---");
+            Console.WriteLine("1. Opción 1");
+            Console.WriteLine("2. Opción 2");
+            Console.WriteLine("3. Opción 3");
+        }
+        static void Main(string[] args)
+        {
+            MostrarMenu();
+        }
+    }
+}
