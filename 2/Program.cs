717d8be448123d01e2270b1e15aac6b50265da10
Andy <Andy@ANDY>
2026-03-22 00:55:46 -0600

    se completo ejercicio 2

---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _2
+{
+    internal class Program
+    {
+        //Mostrar un nombre en pantalla utilizando una función MostrarNombre().
+        static void MostrarNombre()
+        {
+            Console.WriteLine("Mi nombre es Anderson Son");
+        }
+        static void Main(string[] args)
+        {
+            MostrarNombre();
+        }
+    }
+}
