7313da84c0248a083854202e36450b489e0375b6
Andy <Andy@ANDY>
2026-03-22 01:59:44 -0600

    ejercicio 9

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        //Sumar dos números utilizando una función SumarMostrar(int a, int b) y mostrar el resultado.
+        static void SumarMostrar(int a, int b)
+        {
+            int resultado = a + b;
+            Console.WriteLine($"La suma de {a} y {b} es: {resultado}");
+        }
+        static void Main(string[] args)
+        {
+            SumarMostrar(5,2);
+        }
+    }
+}
