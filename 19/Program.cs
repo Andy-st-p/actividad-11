7078b8e426be4f3a5647edcc26f181acf28dbcec
Andy <Andy@ANDY>
2026-03-22 09:09:14 -0600

    ejercicio 19

---
+++ 19/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _19
+{
+    internal class Program
+    {
+        //Determinar si un número es múltiplo de otro utilizando una función EsMultiplo(int n, int m) que retorne verdadero o falso.
+        static bool EsMultiplo(int n, int m)
+        {
+            return n % m == 0;
+        }
+        static void Main(string[] args)
+        {
+            bool resultado= EsMultiplo(1,2);
+            Console.WriteLine(resultado);
+        }
+    }
+}
