bab4006586a6d7a2d15e5c4fb33376169956be75
Andy <Andy@ANDY>
2026-03-22 01:43:42 -0600

    ejercicio 6 completado

---
+++ 6/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _6
+{
+    internal class Program
+    {
+        //Saludar a una persona utilizando una función Saludar(string nombre) que imprima “Hola + nombre”.
+        static void Saludar(string Nombre)
+        {
+            Console.WriteLine("Hola " + Nombre);
+        }
+        static void Main(string[] args)
+        { 
+            
+            Saludar("Anderson Son");
+
+        }
+    }
+}
