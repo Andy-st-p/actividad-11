f8a1aff87092adc5f77c55763aef2f1e310a4596
Andy <Andy@ANDY>
2026-03-22 08:26:44 -0600

    ejercicio 13

---
+++ 13/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _13
+{
+    internal class Program
+    {
+        //Multiplicar dos números utilizando una función Multiplicar(int a, int b) que retorne el resultado.
+        static void Multiplicar(int a, int b)
+        {
+            Console.WriteLine($"El resultado de multiplicar {a} x {b} es: {(a*b)}");
+        }
+        static void Main(string[] args)
+        {
+            Multiplicar(5,6);
+        }
+    }
+}
