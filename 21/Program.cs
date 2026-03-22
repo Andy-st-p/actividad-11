cb3c0271a10504ff44544921b2d942df0c2a8ce2
Andy <Andy@ANDY>
2026-03-22 09:16:57 -0600

    ejercicio 21

---
+++ 21/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _21
+{
+    internal class Program
+    {
+        //Contar la cantidad de vocales dentro de un texto utilizando una función ContarVocales(string texto).
+        static void ContarVocales(string texto)
+        {
+            int contador = 0;
+            string vocales = "aeiouAEIOU";
+            foreach (char c in texto)
+            {
+                if (vocales.Contains(c))
+                {
+                    contador++;
+                }
+            }
+            Console.WriteLine($"El texto '{texto}' tiene {contador} vocales.");
+        }
+        static void Main(string[] args)
+        {
+            ContarVocales("DOMingo");
+        }
+    }
+}
