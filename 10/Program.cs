7f9a7df27e059ee3f156927e28b52868e4a0265f
Andy <Andy@ANDY>
2026-03-22 02:05:16 -0600

    ejercicio 10

---
+++ 10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _10
+{
+    internal class Program
+    {
+        //Imprimir un texto varias veces utilizando una función ImprimirRepetido(string texto, int veces).
+        static void ImprimirRepetido(string texto, int veces)
+        {
+            for (int i = 0; i < veces; i++)
+            {
+                Console.WriteLine(texto);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ImprimirRepetido("hola",6);
+        }
+    }
+}
