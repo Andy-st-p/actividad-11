b2829b804ae641c0026c5a6707e27932a848e1e8
Andy <Andy@ANDY>
2026-03-22 08:43:39 -0600

    ejercicio 15

---
+++ 15/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _15
+{
+    internal class Program
+    {
+        //Determinar si un número es par utilizando una función EsPar(int n) que retorne verdadero o falso.
+         static bool EsPar(int n)
+        {
+            return n % 2 == 0;
+        }
+        static void Main(string[] args)
+        {
+            bool resultado = EsPar(5);
+            Console.WriteLine(resultado);
+        }
+    }
+}
