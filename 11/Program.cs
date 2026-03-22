86a9bf134ddb2a50264c2aac4f2fe8271c845bae
Andy <Andy@ANDY>
2026-03-22 08:20:06 -0600

    ejercicio 11

---
+++ 11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _11
+{
+    internal class Program
+    {
+        //Sumar dos números utilizando una función Sumar(int a, int b) que retorne el resultado.
+        
+        static void Sumar( int a, int b)
+        {
+            Console.WriteLine($"La suma de {a}+{b} es: {(a+b)}");
+        }
+        static void Main(string[] args)
+        {
+            Sumar( 6, 4 );
+        }
+    }
+}
